8789252a2afbb62811d4f8d961ec28d727957e40
Kade Kobussen <kadekobussen50@gmail.com>
2023-05-01 12:04:26 -0700

    finish function for Player.rb and assemble main.rb

--- Classes/Player.rb
+++ Classes/Player.rb
@@ -1,13 +1,22 @@
 class Player
   attr_accessor :score
   attr_accessor :name
-  /answer question/
-  /alive? returns true if lives left/
-  /to_s return name and score/
+  attr_accessor :lives
+  
   def initialize
     puts "What is your name?"
     @name = gets.chomp
-    @score = 3
+    @lives = 3
+    @score = 0
+  end
+  
+  def alive?
+    @lives > 0
+  end
+  
+  def to_s
+    "#{name}: #{@score} points, #{@lives} lives left"
   end
 end
+
 
--- main.rb
+++ main.rb
@@ -1,3 +1,15 @@
-require_relative 'Game'
-require_relative 'Player'
-require_relative 'Question'
+require_relative 'Classes/Game'
+require_relative 'Classes/Player'
+require_relative 'Classes/Question'
+
+player1 = Player.new
+player2 = Player.new
+
+game = Game.new(player1, player2)
+game.play
+
+if game.check_game_over
+  puts "Game over!"
+else
+  puts "Thanks for playing!"
+end

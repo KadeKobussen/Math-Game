6535ff7790d21aae5fc6773e73b3a5c048a9ab71
Kade Kobussen <kadekobussen50@gmail.com>
2023-05-01 12:13:14 -0700

    fix errors in Question class

--- Classes/Question.rb
+++ Classes/Question.rb
@@ -1,10 +1,15 @@
 class Question
   attr_reader :question, :answer
-
+  MAX_LIVES = 3
   def initialize
     num1 = rand(1..99)
     num2 = rand(1..99)
     @question = "#{num1} + #{num2}"
     @answer = num1 + num2
   end
+
+  def text
+    @question
+  end
+
 end
